00ff8c2a3111e4a3995f2b157945ac67971d4186
Albert Too Kipchirchir <tooalbert79@gmail.com>
2023-12-09 23:00:56 +0300

    indices used

--- src/Main.cpp
+++ src/Main.cpp
@@ -41,10 +41,16 @@ int main() {
 	float positions[] = {
 		-0.5f,-0.5f,
 		 0.5f,-0.5f,
-		 0.0f,0.5f	  
+		 0.5f,0.5f,
+		-0.5f,0.5f
+	};
+
+	int indices[] = {
+		0,1,2,
+		0,3,2
 	};
 	
-	unsigned int VAO,VBO;
+	unsigned int VAO,VBO,EBO;
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -53,13 +59,19 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER,VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(positions),positions,GL_STATIC_DRAW);
 
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+
 
 	glEnableVertexAttribArray(0);
 
 	while (!glfwWindowShouldClose(window)) {
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 4);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
